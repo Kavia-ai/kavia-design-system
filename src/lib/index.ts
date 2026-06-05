5c73593107799fcc177032a00ecb486ee0898276
Chris Maxwell <248452085+withchris-kavia@users.noreply.github.com>
2026-06-05 11:06:57 +0000

    Checkpoint 2 - cm25af5dae

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Root library entrypoint for the Kavia Design System package.
+ *
+ * Consumers should import components from the package root:
+ * `import { Button } from "kavia-design-system"`
+ *
+ * Styles are exposed separately via:
+ * `import "kavia-design-system/styles.css"`
+ */
+
+export * from "../kavia";
